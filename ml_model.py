0bef7f2fbd2b338a39d3a4159efd947f473649aa
Tilak L <129746295+tilak-cloudx@users.noreply.github.com>
2025-09-14 00:04:52 +0530

    Update ml_model.py

--- ml_model.py
+++ ml_model.py
@@ -1,17 +1,5 @@
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
 import joblib
-
-# Train a simple model (using sample dataset)
-def train_model():
-    data = pd.read_csv("data/sample_dataset.csv")
-    X = data.drop("label", axis=1)
-    y = data["label"]
-
-    model = RandomForestClassifier()
-    model.fit(X, y)
-
-    joblib.dump(model, "model/trained_model.pkl")
 
 def predict_threat(features: dict):
     model = joblib.load("model/trained_model.pkl")
