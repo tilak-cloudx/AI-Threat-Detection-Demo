96516b093eb6733251f3eea27bad3c1003e85219
Tilak L <129746295+tilak-cloudx@users.noreply.github.com>
2025-09-14 10:39:21 +0530

    Delete utils.py

--- utils.py
+++
@@ -1,9 +0,0 @@
-import random
-
-def generate_fake_traffic():
-    return {
-        "ip": f"192.168.0.{random.randint(1,255)}",
-        "protocol": random.choice(["TCP", "UDP", "ICMP"]),
-        "port": random.randint(20, 8080),
-        "packet_size": random.randint(200, 1500)
-    }
